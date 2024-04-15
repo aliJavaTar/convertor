81d16694781ed2c43968abf92aaa9e9e334ee0d3
a.erfagh <ali77erfagh99@gmail.com>
2024-04-15 15:52:18 +0330

    first commit

---
+++ src/main/java/org/sepeher/convertor/ConvertorApplication.java
@@ -0,0 +1,13 @@
+package org.sepeher.convertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sepeher/convertor/ConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sepeher.convertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
