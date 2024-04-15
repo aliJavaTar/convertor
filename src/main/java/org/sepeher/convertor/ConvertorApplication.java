319e984b746d32e5163a29033c96d112efbde7a5
a.erfagh <ali77erfagh99@gmail.com>
2024-04-15 15:59:42 +0330

    first commit

--- src/main/java/org/sepeher/convertor/ConvertorApplication.java
+++ src/main/java/org/sepeher/convertor/ConvertorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConvertorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConvertorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertorApplication.class, args);
+    }
 
 }
--- src/test/java/org/sepeher/convertor/ConvertorApplicationTests.java
+++ src/test/java/org/sepeher/convertor/ConvertorApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConvertorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
